21e3d040a9bbafca931166f9fffaba3e3db8fd93
wambatcodeeee <feyfey927@gmail.com>
2026-08-28 20:51:09 +0900

    feat: 잘못된 경로 폴백과 페이지 전환 애니메이션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useParams } from 'react-router-dom'
 import { SiteLayout } from '@/components/layout/SiteLayout'
 import ThemeDashboardPage from '@/pages/ThemeDashboardPage'
 import ThemeDetailPage from '@/pages/ThemeDetailPage'
@@ -7,16 +7,23 @@ import CorpGraphPage from '@/pages/CorpGraphPage'
 import ThemeListPage from '@/pages/ThemeListPage'
 import StockListPage from '@/pages/StockListPage'
 
+function ThemesAliasRedirect() {
+  const { themeId } = useParams()
+  return <Navigate to={`/theme/${themeId}`} replace />
+}
+
 function App() {
   return (
     <Routes>
       <Route element={<SiteLayout />}>
         <Route path="/" element={<ThemeDashboardPage />} />
         <Route path="/themes" element={<ThemeListPage />} />
+        <Route path="/themes/:themeId" element={<ThemesAliasRedirect />} />
         <Route path="/theme/:themeId" element={<ThemeDetailPage />} />
         <Route path="/stocks" element={<StockListPage />} />
         <Route path="/stock/:stockCode" element={<StockDetailPage />} />
         <Route path="/graph" element={<CorpGraphPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   )
